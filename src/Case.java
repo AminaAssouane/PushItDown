0c8fd2adf9a97ce53bdbf5e7a3209572a3e73d55
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-15 05:31:45 +0100

    Ajouter 'src/Case.java'

---
+++ src/Case.java
@@ -0,0 +1,24 @@
+
+public class Case {
+	
+	private int axeX;
+	private int axeY;
+	private int axeZ;
+	private boolean boule;
+	private boolean sortie;
+	
+	
+	// Getters //
+	public int getAxeX(){
+		return this.axeX;
+	}
+	
+	public int getAxeY(){
+		return this.axeY;
+	}
+	
+	public int getAxeZ(){
+		return this.axeZ;
+	}
+
+}
