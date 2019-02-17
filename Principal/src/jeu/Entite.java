82cc8449dd4df2fdccb3b92b992688cd6fe7c3f5
Julien BERSAUTER <julien.bersauter@orange.fr>
2019-02-17 22:33:08 +0100

    Quelques corrections

--- Principal/src/jeu/Entite.java
+++ Principal/src/jeu/Entite.java
@@ -4,5 +4,12 @@ public interface Entite {
 	public int getX();
 	public int getY();
 	public int getZ();
-        public void deplacer(int x, int y, int z);
+
+    /**
+     * deplace l'entite sur la case x, y ,z
+     * @param x
+     * @param y
+     * @param z
+     */
+    public void deplacer(int x, int y, int z);
 }
