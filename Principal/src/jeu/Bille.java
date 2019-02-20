5ea5b10e26faf3ce311b2135b3590bc5920158be
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-20 04:59:49 +0100

    Corrections

--- Principal/src/jeu/Bille.java
+++ Principal/src/jeu/Bille.java
@@ -2,19 +2,17 @@ package jeu;
 
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 public class Bille extends Bloc{
 	int x,y,z;
-	Bille(int x,int y,int z,JFrame f){
+	Bille(int x,int y,int z,JLayeredPane f){
 		this.x=x;
 		this.y=y;
 		this.z=z;
 		jl= new JLabel(new ImageIcon("BALL.png"));
 		jl.setBounds(x, y, 40, 40);
-		f.add(jl);
+		f.add(jl,10000,1);
 	}
 	void moveleft(){
 		int x2=x-20;
