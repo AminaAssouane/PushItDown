5129e31c8510a49f33f6dcba5e6030c78d0070c8
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-20 04:57:53 +0100

    Corrections

--- Principal/src/jeu/Main.java
+++ Principal/src/jeu/Main.java
@@ -1,5 +1,6 @@
 package jeu;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;  
@@ -9,19 +10,27 @@ public class Main {
 	
 	public static void main(String args[]) {
 //		Sortie s[];
-		JFrame f = new JFrame();  
-		Bille b = new Bille(200, 200, 1,f);
+		JFrame f = new JFrame(); 
+		JPanel p = new JPanel();
+		f.setBounds(100, 100, 450, 300);
+		p.setLayout(null);
+		
+		JLayeredPane pa = new JLayeredPane();
+		pa.setPreferredSize(new Dimension(500, 500));
+			
+		Bille b = new Bille(200, 78, 1, pa);
 //		int moves;
-		new Grid(3,8,8,f,new Level());
-		f.setSize(1000, 600);
-		f.setLayout(null);
+		new Grid(pa,new Level(),1);
+		f.setSize(500, 500);
+		
+		f.getContentPane().add(pa, BorderLayout.CENTER);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		f.setVisible(true);
 		
-		for(int i = 0;i<8;i++) {
+		/*for(int i = 0;i<8;i++) {
 			b.moveright();
 //			f.update(jl.getGraphics());
-		}
+		}*/
 	}
 
 }
