38ffd8e68d1a8a01da0bed00c29421f3d60264c4
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-21 21:09:53 +0100

    Classe qui gère les déplacement avec les touches du clavier

--- Principal/src/jeu/Deplacement.java
+++ Principal/src/jeu/Deplacement.java
@@ -5,17 +5,35 @@ import java.awt.event.KeyListener;
 
 public class Deplacement implements KeyListener{
 
+	Plateau plat;
+	Bille b;
+	public Deplacement(Plateau plat, Bille b){
+		this.plat = plat;
+		this.b = b;
+	}
+	
 	@Override
 	// Fonction qui sert à déplacer la bille
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int touche = e.getKeyCode();
 		switch (touche){
-		case KeyEvent.VK_LEFT :
-			System.out.println("testGauche");
 		case KeyEvent.VK_RIGHT :
+			Plateau.Deplacement d = plat.new Deplacement(b, b.getX()+1, b.getY(), b.getZ());
+			plat.deplacement(d);
+			break;
+		case KeyEvent.VK_LEFT :
+		    d = plat.new Deplacement(b, b.getX()-1, b.getY(), b.getZ());
+			plat.deplacement(d);
+			break;
 		case KeyEvent.VK_UP :
+			d = plat.new Deplacement(b, b.getX(), b.getY()-1, b.getZ());
+			plat.deplacement(d);
+			break;
 		case KeyEvent.VK_DOWN :
+			d = plat.new Deplacement(b, b.getX(), b.getY()+1, b.getZ());
+            plat.deplacement(d);
+			break;
 			default : 
 				break;
 		}
