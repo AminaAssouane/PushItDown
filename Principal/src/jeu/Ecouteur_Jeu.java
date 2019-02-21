01d0466a69101d092b12ebd35defb4921898bdb9
Julien Bersauter <jbersa88@s06.script.univ-paris-diderot.fr>
2019-02-21 12:20:52 +0100

    Modif rapide

---
+++ Principal/src/jeu/Ecouteur_Jeu.java
@@ -0,0 +1,54 @@
+package jeu;
+
+public class Controleur_Jeu implements Ecouteur_Tour, Ecouteur_DebutPartie  {
+    private Controleur_Tour controleurtour = new Controleur_Tour();
+    private Controleur_DebutPartie debutpartie = new Controleur_DebutPartie();
+    @Override
+    private Jeu jeu
+        public Controleur_Jeu (Jeu j){
+        this.jeu = j;
+    }
+    
+    public void tour() {
+        int n = 0;
+        ArrayList <Deplacement> deplacement  = this.j.getChoixactuel();
+        int taille = deplacement.size()-1;
+        
+        // ton interface graphique qui modifie n avec les choix dans jeu
+        // un bouton qui modifie n a -1 pour passer le tour
+        // un bouton qui modifie n a -2 pour abandonner la partie
+        // un bouton qui modifie n en fonction des choix dans la liste
+        // Jeu.choixactuel.get(int n)
+        
+        controleurtour.choisir(n);
+    }
+
+    @Override
+    public void fintour() {
+        // Lire le plateau de jeu et afficher les informations actualisees
+
+    }
+
+    @Override
+    public void creerJoueur() {
+        
+
+    }
+
+    @Override
+    public void choisirCarte() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void choisirRegle() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void creerBille() {
+        // TODO Auto-generated method stub
+    }
+}
