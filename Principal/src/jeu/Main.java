91e26b4e34bd7dcc817a1168c99d1edc3c050b2f
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-21 10:09:03 +0100

    Ajout de commentaires

--- Principal/src/jeu/Main.java
+++ Principal/src/jeu/Main.java
@@ -11,17 +11,21 @@ public class Main {
 	public static void main(String args[]) {
 //		Sortie s[];
 		JFrame f = new JFrame(); 
-		JPanel p = new JPanel();
 		f.setBounds(100, 100, 450, 300);
-		p.setLayout(null);
 		
 		JLayeredPane pa = new JLayeredPane();
 		pa.setPreferredSize(new Dimension(500, 500));
 			
 		Bille b = new Bille(200, 78, 1, pa);
-//		int moves;
+
+      // Dans la fonction Grid on a qu'à indiqué le niveau (ici le niveau est 1, et il n'y a que 2 niveaux pour 
+      // l'instant.
+      // Les niveaux sont stockés dans la classe "Level", et sont choisi à partir de la fonction "niveau"
+      // L'utilisateur n'a pas besoin de spécifié les coordonnées x, y et z du niveau, celles si sont donné par la
+      // classe "Level"
+      
 		new Grid(pa,new Level(),1);
-		f.setSize(500, 500);
+		f.setSize(500, 500); 
 		
 		f.getContentPane().add(pa, BorderLayout.CENTER);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
