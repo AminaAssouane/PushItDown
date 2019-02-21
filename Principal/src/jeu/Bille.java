5e076c4499e1fddc6d5972b3f84dddbe1bb747a1
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-21 21:06:01 +0100

    Ajout de fonction deplacer + corrections

--- Principal/src/jeu/Bille.java
+++ Principal/src/jeu/Bille.java
@@ -2,40 +2,55 @@ package jeu;
 
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
-public class Bille extends Bloc{
+public class Bille extends Bloc implements Entite{
 	int x,y,z;
+	JLayeredPane f;
+	public Bille(){};
+	public Bille(Byte b){
+		super(b);
+	}
+	
 	Bille(int x,int y,int z,JLayeredPane f){
+		this.f=f;
 		this.x=x;
 		this.y=y;
 		this.z=z;
-		jl= new JLabel(new ImageIcon("BALL.png"));
-		jl.setBounds(x, y, 40, 40);
-		f.add(jl,10000,1);
+		jl= new JLabel(new ImageIcon("C:\\Users\\HP\\Desktop\\Semestre6\\ProjetL2\\ProjetJ\\src\\BALL.png"));
+		jl.setBounds(200, 200-((z+1)*20 + 2), 40, 40);
+		f.add(jl,1000,1);
 	}
-	void moveleft(){
-		int x2=x-20;
-		while(x>x2) {
-			waits();
-			x-=2;
-			y--;
-		}
-	}
-	void moveup(){}
-	void movedown(){}
-	void moveright(){
-		int x2=x+20;
-		while(x<x2) {
-			waits();
-			x+=2;
-			y++;
-			jl.setBounds(x, y, 40, 40);
-			System.out.println("ok");
-		}
+	
+	public void deplacer(int x,int y, int z){
+		jl.setBounds(200+(x*20)-(y*20), 200 + (x * 10) + (10 * y)-((z+1)*20 + 2), 40, 40);
+		f.add(jl,10000,1);	
 	}
 
-	static void waits() {//pour tester
-//		System.nanoTime()
+  /** Getters and Setters **/
+	
+	public int getX(){
+		return this.x;
+	}
+	public int getY(){
+		return this.y;
+	}
+	public int getZ(){
+		return this.z;
+	}
+	
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	public void setZ(int z){
+		this.z = z;
 	}
 }
