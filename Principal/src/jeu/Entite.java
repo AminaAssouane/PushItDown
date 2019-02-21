997735a22c307035dd3cd1a6589d215ba4347761
Amina Assouane <amina.assouane@etu.univ-paris-diderot.fr>
2019-02-21 21:10:37 +0100

    Ajout des setteurs

--- Principal/src/jeu/Entite.java
+++ Principal/src/jeu/Entite.java
@@ -1,9 +1,6 @@
 package jeu;
 
 public interface Entite {
-	public int getX();
-	public int getY();
-	public int getZ();
 
     /**
      * deplace l'entite sur la case x, y ,z
@@ -12,4 +9,13 @@ public interface Entite {
      * @param z
      */
     public void deplacer(int x, int y, int z);
+    
+    /** Getters and Setters **/
+	public int getX();
+	public int getY();
+	public int getZ();
+	public void setX(int x);
+	public void setY(int y);
+	public void setZ(int z);
+
 }
